825b4052a0c3f4a02a64875bfccd58c2e091f48b
somecallmejosh <josh@thebrileys.com>
2024-12-13 14:21:24 -0500

    Extend nuxt ui from base layer.

--- base-layer/nuxt.config.js
+++ base-layer/nuxt.config.js
@@ -5,6 +5,11 @@ export default defineNuxtConfig({
       pathPrefix: false,
     },
   ],
-
+  runtimeConfig: {
+    public: {
+      API_BASE_URL: process.env.API_BASE_URL,
+    },
+  },
+  extends: ['@nuxt/ui-pro'],
   modules: ['@nuxt/ui'],
 });
